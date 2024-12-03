a24dc310a6b7e38b10f1b78922fa3cc90005592a
asadd2723 <asadu3040@gmail.com>
2024-12-04 01:10:51 +0500

    some file

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const math = require('./math.js')
 console.log('Hello World!')
+console.log(math(1,2))
 console.log("git config --global user.name 'Your Name'")
 const age = 23 // age is a nu
 console.log(age)
